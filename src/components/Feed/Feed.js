a3ae298856c942cd990287b918a61e1097a44bc4
Carie Fisher <carie.fisher@deque.com>
2020-07-28 12:38:00 -0500

    npm updates

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -25,6 +25,7 @@ const Feed = ({ edges }: Props) => (
         <h2 className={styles['feed__item-title']}>
           <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
         </h2>
+        <p className={styles['feed__item-image']}><img>{edge.node.frontmatter.socialImage}</img></p>
         <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
         <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read more &#8594;</Link>
       </div>
