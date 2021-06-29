cdef0bd293f7f4471a23ce483e02766d637755da
Carie Fisher <carie.fisher@deque.com>
2021-06-29 16:34:31 -0500

    feature

--- src/components/teasers/PostNext/index.js
+++ src/components/teasers/PostNext/index.js
@@ -8,28 +8,28 @@ const PostTeaser = (props) => {
   return(
     <div className='post-next' key={props.node.fields.slug}>
       <div className='post-next__item-meta'>
-        <h2>Next Talk</h2>
-        <h3 className="post-next__item-speaker">{props.node.frontmatter.subtitle}</h3>
-        <time className={'post-next__item-date'} dateTime={moment(props.node.frontmatter.date).format('MMMM YYYY')}>
-          {moment(props.node.frontmatter.date).format('MMMM YYYY')}
-        </time>
-        <h3 className='post-next__item-title'>
-          <Link className='post__item-title-link' to={props.node.fields.slug}>{props.node.frontmatter.title}</Link>
-        </h3>
-      </div>
-      <div className='post-next__item-content'>
-
         <div className='post-next__item-image'>
             <img
-              src={props.node.frontmatter.socialImage}
-              className='social-image-next'
-              alt={props.node.frontmatter.subtitle}
+            src={props.node.frontmatter.socialImage}
+            className='social-image-next'
+            alt={props.node.frontmatter.subtitle}
             />
           </div>
-        <p className='post-next__item-description'>{props.node.frontmatter.description}
-        <br />
-        <Link className='post-next__item-readmore' to={props.node.fields.slug}>Read more<span class="sr-only"> about {props.node.frontmatter.title}</span> &#8594;</Link>
-        </p>
+          <div className='post-next__item-content'>
+            <div>
+              <h2 className={'post-next__item-date'} dateTime={moment(props.node.frontmatter.date).format('MMMM YYYY')}>
+                {moment(props.node.frontmatter.date).format('MMMM YYYY')}
+              </h2>
+              <h3 className="post-next__item-speaker">{props.node.frontmatter.subtitle}</h3>
+              <h3 className='post-next__item-title'>
+                <Link className='post-next__item-title-link' to={props.node.fields.slug}>{props.node.frontmatter.title}</Link>
+              </h3>
+            </div>
+            <p className='post-next__item-description'>{props.node.frontmatter.description}
+              <br /><br />
+              <Link className='post-next__item-readmore' to={props.node.fields.slug}>Read more<span class="sr-only"> about {props.node.frontmatter.title}</span> &#8594;</Link>
+            </p>
+        </div>
       </div>
 
       <hr class="hr-last" />
