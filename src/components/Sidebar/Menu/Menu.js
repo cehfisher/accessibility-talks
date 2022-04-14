1603561c8188850baf115bfd00df9e90ff8ce5b6
mark-casias <mark.casias@kanopistudios.om>
2022-04-14 12:26:02 -0600

    Adding store to menu (and external links)

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -3,25 +3,20 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Menu.module.scss';
 
-type Props = {
-  menu: {
-    label: string,
-    path: string
-  }[]
-};
-
-const Menu = ({ menu }: Props) => (
+const Menu = ({ menu }) => (
   <nav className={styles['menu']}>
     <ul className={styles['menu__list']}>
       {menu.map((item) => (
         <li className={styles['menu__list-item']} key={item.path}>
-          <Link
+          { item.external && <a href={item.path} target="_blank" rel="noopener noreferrer">{item.label}</a>}
+          { !item.external && (<Link
             to={item.path}
             className={styles['menu__list-item-link']}
             activeClassName={styles['menu__list-item-link--active']}
           >
             {item.label}
-          </Link>
+          </Link>)
+          }
         </li>
       ))}
     </ul>
