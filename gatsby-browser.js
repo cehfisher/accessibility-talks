d858b17185fa8596827dda1c517582d89eda2ec3
Mark Casias <markie@teampoop.com>
2024-05-16 17:11:02 -0600

    again with the cache...

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,5 @@
 require('./src/assets/scss/init.scss');
 require('./static/css/prismjs/theme.min.css');
 import "@fontsource/montserrat"
+
+exports.onServiceWorkerUpdateFound = () => window.location.reload(true);
